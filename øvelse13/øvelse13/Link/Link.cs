354ea0dca7f5ad152c2b6a950161a7344adc891d
Lazoor <lasse_siv@hotmail.com>
2015-11-11 16:24:05 +0100

    Added the project and corrected the code a little.

--- øvelse13/øvelse13/Link/Link.cs
+++ øvelse13/øvelse13/Link/Link.cs
@@ -1,33 +1,17 @@
 using System;
 using System.IO.Ports;
 
-/// <summary>
-/// Link.
-/// </summary>
 namespace Linklaget
 {
-	/// <summary>
-	/// Link.
-	/// </summary>
 	public class Link
 	{
-		/// <summary>
-		/// The DELIMITE for slip protocol.
-		/// </summary>
+		const int MAXFILESIZE = 1000;
 		const byte DELIMITER = (byte)'A';
-		/// <summary>
-		/// The buffer for link.
-		/// </summary>
 		private byte[] buffer;
-		/// <summary>
-		/// The serial port.
-		/// </summary>
 		SerialPort serialPort;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="link"/> class.
-		/// </summary>
-		public Link (int BUFSIZE)
+
+		public Link (int MAXFILESIZE)
 		{
 			// Create a new SerialPort object with default settings.
 			serialPort = new SerialPort("/dev/ttyS1",115200,Parity.None,8,StopBits.One);
@@ -35,35 +19,93 @@ namespace Linklaget
 			if(!serialPort.IsOpen)
 				serialPort.Open();
 
-			buffer = new byte[(BUFSIZE*2)+2];
+			buffer = new byte[(MAXFILESIZE*2)+2];
 		}
 
-		/// <summary>
-		/// Send the specified buf and size.
-		/// </summary>
-		/// <param name='buf'>
-		/// Buffer.
-		/// </param>
-		/// <param name='size'>
-		/// Size.
-		/// </param>
 		public void send (byte[] buf, int size)
 		{
-	    	// TO DO Your own code
+			byte[] temp;
+			int tempCount = 0;
+
+			for (int i=0; i == size; i++) {
+				if (i > 0 && i < size) {
+					if (buf [i] == 'A') {
+						temp [tempCount] = (byte)'B';
+						temp [tempCount + 1] = (byte)'C';
+						tempCount++;
+					} else if (buf [i] == 'B') {
+						temp [bufCount] = (byte)'B';
+						temp [bufCount + 1] = (byte)'D';
+						tempCount++;
+					} else {
+						temp [tempCount] = buf [i];
+					}
+				} else {
+					temp [tempCount] = DELIMITER;
+				}
+				tempCount++;
+			}
+			serialPort.Write (buf, 0, size);
 		}
 
-		/// <summary>
-		/// Receive the specified buf and size.
-		/// </summary>
-		/// <param name='buf'>
-		/// Buffer.
-		/// </param>
-		/// <param name='size'>
-		/// Size.
-		/// </param>
 		public int receive (ref byte[] buf)
 		{
-	    	// TO DO Your own code
+			bool wait = false;
+			// Total size counter
+			int bufReadSize = 0;
+			// i = temporary buffer iterator, bufCount = buf iterator
+			int i = 0, bufCount = 0;
+			byte[] temp;
+
+			// ################  DEBUG  OUTCOMMENT IF NEEDED ##############
+			Console.WriteLine ("Receive function started...");
+			//#############################################################
+
+			// Receive while-loop
+			while (wait != true) {								
+				// serialPort.Read(buffer, offset, maxSizeBuffer)
+				bufReadSize += serialPort.Read (temp[i], 0, 1); 
+
+				// ################  DEBUG  OUTCOMMENT IF NEEDED ##############
+				Console.WriteLine ("Reading directly from link-layer:\n");
+				Console.Write (temp [i]);
+				//#############################################################
+
+				// If not buf[0] == 'A' -> exit
+				if (temp [0] != DELIMITER) {					
+					Console.Error.WriteLine ("Forkert frame....!");
+					return 1;
+				} 
+				// If buf[i] == 'A' -> reading done...
+				if (i > 0 && buf [i] == DELIMITER) {			
+					Console.WriteLine ("Reading done...");
+					// exit while.
+					wait = true;	
+				} else if (i > MAXFILESIZE){
+					// Timed out.
+					return 2;									
+				}
+				// Incrementing for each byte read.
+				i++;											
+			}
+
+			for (int j = 0; j < bufReadSize; ++j) {
+				if (j > 0 && j < bufReadSize) {
+					if (temp [j] == 'B' && temp [j+1] == 'C') {	// Replace 'BC' with 'A'
+						buf [bufCount] = 'A';
+						j++;
+					} else if (temp [j] == 'B' && temp [j+1] == 'D') { // Replace 'BD' with 'B'
+						buf [bufCount] = 'B';
+						j++;
+					} else {									// Anything else 
+						buf [bufCount] = temp [j];
+					}
+				} else {										// Frame handling.
+					buf [j] = temp [j];
+				}
+				bufCount++;										// Compressing readBuf to actual size buf
+			}
+			return 0;										
 		}
 	}
 }
